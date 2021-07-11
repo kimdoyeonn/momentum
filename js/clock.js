16856e4689b86b4b9f3f13c61a8cc16df8bed0cd
doyeon <doyeon311@gmail.com>
2021-07-12 02:45:16 +0900

    add date

--- js/clock.js
+++ js/clock.js
@@ -1,4 +1,5 @@
 const clock = document.querySelector("h2#clock");
+const today = document.querySelector("#date");
 
 function getClock() {
   const date = new Date();
@@ -6,6 +7,13 @@ function getClock() {
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
   clock.innerText = `${hours}:${minutes}:${seconds}`;
+
+  const year = String(date.getFullYear());
+  const month = String(date.getMonth() + 1);
+  const day = String(date.getDate());
+  const dayOfWeek = String(date.getDay());
+  const week = ["일", "월", "화", "수", "목", "금", "토"];
+  today.innerText = `${year}년 ${month}월 ${day}일 ${week[dayOfWeek]}요일`;
 }
 
 getClock();
